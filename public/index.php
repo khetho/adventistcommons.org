9e26adf3f1a740c47dcc99c79d7e44c57ab437b6
Vincent BEAUVIVRE <vincent.beauvivre@smile.fr>
2019-11-07 10:54:54 +0100

    Remove code igniter system code, and require it through composer

--- public/index.php
+++ public/index.php
@@ -97,7 +97,8 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = realpath(dirname(__FILE__).'/../codeIgniterSystem');
+
+	$system_path = realpath(dirname(__FILE__).'/../application/vendor/codeigniter/framework/system');
 
 /*
  *---------------------------------------------------------------
